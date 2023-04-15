9f1c43549b143888005058d2d6138d1f03c2532a
mizutanimeen <dongxingshuigu@gmail.com>
2023-04-15 16:28:44 +0900

    react router

--- react/app/src/App.tsx
+++ react/app/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
+import NotFound from './NotFound';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
